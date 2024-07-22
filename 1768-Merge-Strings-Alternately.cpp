2329b644450797522eb6d77f32988613aa139ceb
Bodour Mohamed <bodourmohamed18@gmail.com>
2024-07-22 04:29:39 +0300

    [1768] [Time Beats: 100.00%] [Memory Beats: 23.38%] - LeetPush

---
+++ 1768-Merge-Strings-Alternately.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i = 0;
+        int j = 0;
+        string res;
+        while (i < word1.length() && j < word2.length()) {
+            res += word1[i++];
+            res += word2[j++];
+        }
+        while (i < word1.length()) {
+            res += word1[i++];
+        }
+
+        while (j < word2.length()) {
+            res += word2[j++];
+        }
+
+        return res;
+    }
+
+};
