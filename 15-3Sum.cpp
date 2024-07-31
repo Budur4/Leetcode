2195acb25b71178647aef4caba3b39dcf9ef23b8
Bodour Mohamed <bodourmohamed18@gmail.com>
2024-07-31 07:09:39 +0300

    [15] [Time Beats: 60.51%] [Memory Beats: 63.68%] - LeetPush

---
+++ 15-3Sum.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
+
+            int j = i + 1, k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[j], nums[k]});
+                    while (j < k && nums[j] == nums[j + 1]) j++; // skip duplicates
+                    while (j < k && nums[k] == nums[k - 1]) k--; // skip duplicates
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
